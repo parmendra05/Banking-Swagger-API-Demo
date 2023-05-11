d3ccb5189de7fd9fa940a102f4b9a8e9d17ab6a6
parmendra kumar <parmendrakumar718@gmail.com>
2023-05-11 18:09:19 +0530

    updated

---
+++ src/main/java/com/pack/dto/CustomerRequestPayload.java
@@ -0,0 +1,20 @@
+package com.pack.dto;
+
+public class CustomerRequestPayload {
+	String customerName;
+	String address;
+	Integer ballance;
+
+	public String getCustomerName() {
+		return customerName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public Integer getBallance() {
+		return ballance;
+	}
+
+}
